67757e1d02413b78b57f5acb0587591281f0df3c
Álvaro Brito <alvaro.brito@bq.com>
2015-04-16 16:17:35 +0200

    Travis configuration fixed again

--- travis-runner.sh
+++ travis-runner.sh
@@ -13,5 +13,5 @@ then
   git init && \
   git add . && \
   git commit -m "deploy" && \
-  git push --force --quiet "https://${GH_TOKEN}@github.com/bq/penguin-doc" HEAD:gh-pages > /dev/null 2>&1
+  git push --force --quiet "https://${GH_TOKEN}@github.com/bq/penguin-doc" master:gh-pages > /dev/null 2>&1
 fi
